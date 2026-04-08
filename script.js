fd8a03208f31c77c208e340980abf755f854a6fe
APDarkness <trofayiscool@gmail.com>
2026-04-07 22:07:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
 fetch("posts.json")
-  .then(res => {
-    if (!res.ok) throw new Error("Failed to load posts.json");
-    return res.json();
-  })
+  .then(res => res.json())
   .then(posts => {
     const container = document.getElementById("posts");
 
-    if (!container) {
-      console.error("Missing #posts container in HTML");
-      return;
-    }
-
-    posts.slice().reverse().forEach(post => {
+    posts.reverse().forEach(post => {
       const div = document.createElement("div");
       div.className = "post";
 
       div.innerHTML = `
-        <a href="post.html?id=${post.id}" class="post-link">
+        <a href="post.html?id=${post.id}">
           <h2>${post.title}</h2>
-          <p class="date">${post.date}</p>
-          <p>${post.preview}</p>
         </a>
+        <p class="date">${post.date}</p>
+        <p>${post.preview}</p>
       `;
 
       container.appendChild(div);
     });
-  })
-  .catch(err => console.error("Error loading posts:", err));
+  });
