61b09e9fac6447fc7816d6b6131e27a65a2b5202
APDarkness <trofayiscool@gmail.com>
2026-04-07 21:36:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,19 @@ fetch("posts.json")
   .then(posts => {
     const container = document.getElementById("posts");
 
-    posts.reverse().forEach(post => {
+    posts.slice().reverse().forEach(post => {
       const div = document.createElement("div");
       div.className = "post";
 
       div.innerHTML = `
-        <a href="post.html?id=${post.id}">
+        <a href="post.html?id=${post.id}" class="post-link">
           <h2>${post.title}</h2>
+          <p class="date">${post.date}</p>
+          <p>${post.preview}</p>
         </a>
-        <p class="date">${post.date}</p>
-        <p>${post.preview}</p>
       `;
 
       container.appendChild(div);
     });
   });
+
